5e5e2d708612ef964192c0eee9f3b709f2ce3e09
ilham-2001 <arizqyakbar@gmail.com>
2023-10-06 14:49:01 +0700

    completed MQTT functionality

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import { useEffect, useState } from 'react';
+import Frame from './components/Frame';
 
 function App() {
-  const [imageBase64, setImageBase64] = useState(null);
-
-  const base64ToImage = (base64String) => {
-    const img = new Image();
-    img.src = `data:image/jpeg;base64,${base64String}`;
-    return img;
-  };
-
-  const brokerUrl = 'ws://127.0.0.1:9001/mqtt';
-
-  let client = mqtt.connect(brokerUrl); // create a client
-  const topic = '/labsyms/image';
-
-  useEffect(() => {
-    // Connect to the MQTT broker
-    client.on('connect', () => {
-      console.log('Connected to MQTT broker');
-      client.subscribe(topic);
-    });
-
-    // Handle incoming messages
-    client.on('message', (topic, message) => {
-      setImageBase64(message.toString());
-    });
-
-    // Cleanup on component unmount
-    return () => {
-      client.end(); // Close the MQTT connection
-    };
-  }, [client, topic]);
-
 
   return (
     <div className="App">
       <h2>Frame</h2>
+      <Frame/>
     </div>
   );
 }
--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,16 +1,50 @@
-import * as mqtt from 'mqtt/dist/mqtt.min'
+import * as mqtt from 'mqtt/dist/mqtt.min';
 import { useEffect, useState } from 'react';
 
-const Frame = function (){
-    const [imageBase64, setImageBase64] = useState(null);
+const brokerUrl = 'ws://127.0.0.1:9001/mqtt';
+const topic = '/labsyms/image';
 
-    return (
-        {imageBase64 && (
-            <img
-              src={`data:image/jpeg;base64,${imageBase64}`}
-              alt="Base64"
-              width="300"
-            />
-          )}
-    )
-}
+
+const Frame = function () {
+  const [imageBase64, setImageBase64] = useState(null);
+
+  const base64ToImage = (base64String) => {
+    const img = new Image();
+    img.src = `data:image/jpeg;base64,${base64String}`;
+    return img;
+  };
+
+  const client = mqtt.connect(brokerUrl); // create a client
+
+  useEffect(() => {
+    // Connect to the MQTT broker
+    client.on('connect', () => {
+      console.log('Connected to MQTT broker');
+      client.subscribe(topic);
+    });
+
+    // Handle incoming messages
+    client.on('message', (topic, message) => {
+      setImageBase64(message.toString());
+    });
+
+    // Cleanup on component unmount
+    return () => {
+      client.end(); // Close the MQTT connection
+    };
+  }, [client, topic]);
+
+  return (
+    <div>
+      {imageBase64 && (
+        <img
+          src={`data:image/jpeg;base64,${imageBase64}`}
+          alt='Base64'
+          width='300'
+        />
+      )}
+    </div>
+  );
+};
+
+export default Frame;
